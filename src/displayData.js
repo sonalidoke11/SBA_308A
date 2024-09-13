f95d10c1598682015ff730c32bd8f3c20c68faea
sonalidoke11 <sonalidoke810@gmail.com>
2024-09-13 09:31:31 -0400

    display fetched temparature on UI

--- src/displayData.js
+++ src/displayData.js
@@ -1,16 +1,18 @@
 export async function displayWeather(data) {
     //console.log(data)
+
     const weatherResult = document.getElementById('weather-result');
     const data1 = await data;
-    console.log(data1);
-    const temperature = data.current_weather.temperature;
-    console.log(temperature);
-    
-    
-    if (data && data.hourly && data.hourly.temperature_2m) {
-        const temperatures = data.hourly.temperature_2m.map(temp => `<p>Temperature: ${temp}°C</p>`).join('');
-        weatherResult.innerHTML = `<h2>Weather Data</h2>${temperatures}`;
-    } else {
+
+    //console.log(data1);
+   
+    if(data1 !== null)
+    {
+        const temperature = data1.current_weather.temperature;
+        const temperature1 = (`<p>Temperature: ${temperature}°C</p>`);
+        weatherResult.innerHTML = `<h2>Weather Data</h2>${temperature1}`;
+    }else{
         weatherResult.innerHTML = '<p>No weather data available.</p>';
     }
+
 }
